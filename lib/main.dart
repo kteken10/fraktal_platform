5716c9f31289cc57a1926efbff543ed683d43180
KEVIN <francis.dissang@chips-analytics.com>
2024-10-09 10:14:26 +0100

    Initialised Color of The Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constants/colors.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,34 +9,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const ColorScheme colorScheme = ColorScheme(
+      primary: AppColors.primaryColor,
+      secondary: AppColors.secondaryColor,
+      tertiary: AppColors.ternaryColor,
+      surface: AppColors.backColor,
+      error: AppColors.backColorFine,
+      onPrimary: AppColors.primaryColor,
+      onSecondary: AppColors.secondaryColor,
+      onSurface: AppColors.backColor,
+      onError: Color.fromARGB(255, 214, 51, 51),
+      brightness: Brightness.light, 
+    
+    );
+
     return MaterialApp(
       title: 'FrakTal PlatFrom Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: colorScheme,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -63,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
      
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
     
         title: Text(widget.title),
       ),
