58c1bbf7e2b6333fccd69ac4d47314c2c3cf130b
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2024-10-16 15:34:50 +0100

    Update

--- lib/ui/avatar_icon.dart
+++ lib/ui/avatar_icon.dart
@@ -14,6 +14,7 @@ class SocialIcon extends StatefulWidget {
   });
 
   @override
+  // ignore: library_private_types_in_public_api
   _SocialIconState createState() => _SocialIconState();
 }
 
--- lib/ui/tab_service.dart
+++ lib/ui/tab_service.dart
@@ -14,6 +14,7 @@ class TabService extends StatefulWidget {
   });
 
   @override
+  // ignore: library_private_types_in_public_api
   _TabServiceState createState() => _TabServiceState();
 }
 
