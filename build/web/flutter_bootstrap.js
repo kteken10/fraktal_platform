510b0aff594bc0fa7df52c423a8527736dded901
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2024-10-18 01:45:33 +0100

    update Version to Deploy

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2800933064"
+    serviceWorkerVersion: "261215794"
   }
 });
