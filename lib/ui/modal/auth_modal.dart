61c5d7b25ace55e942759cd7796241aabc28b5f7
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2024-10-18 01:17:09 +0100

    Adding Modale for User Authentification

--- lib/ui/modal/auth_modal.dart
+++ lib/ui/modal/auth_modal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../constants/colors.dart';
 
 class AuthModal extends StatelessWidget {
   const AuthModal({super.key});
@@ -11,24 +13,31 @@ class AuthModal extends StatelessWidget {
       ),
       child: Container(
         padding: const EdgeInsets.all(16.0),
-        width: 400,
-        height: 500,
-        decoration: BoxDecoration(
-          color: Colors.white, // Définir la couleur de fond ici
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Center( // Centrer le contenu (peut-être ajouter d'autres widgets plus tard)
+        width: 300,
+        height: 400,
+        child: SingleChildScrollView(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              // Espace pour des widgets futurs
-              SizedBox(height: 20), // Espace vide
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(); // Ferme la modale
-                },
-                child: const Text('Fermer'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Mon Profil',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: const Icon(FontAwesomeIcons.xmark),
+                    color: AppColors.primaryColor,
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Ferme la modale
+                    },
+                  ),
+                ],
               ),
+              SizedBox(height: 16),
+              // Contenu vierge
+              SizedBox(height: 16), // Espace pour l'esthétique
             ],
           ),
         ),
