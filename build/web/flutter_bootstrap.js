fdc36f2800d43eea52da1ee96039365800b12b82
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2024-10-21 12:29:10 +0100

    upgrade home page image

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2721429533"
+    serviceWorkerVersion: "2382533331"
   }
 });
